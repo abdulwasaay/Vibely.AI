b539c1b68898d5df834cd13932f6019101cfb7c3
abdul wasay <wasaysaleem071@gmail.com>
2025-07-29 13:28:17 -0700

    done

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -3,8 +3,10 @@ import dynamic from "next/dynamic";
 import ButtonLatest from "./ButtonLatest";
 import { withClientOnly } from "./ClientOnlyComponent";
 import ClientThemeProvider from "./parentThemeComponent/clientThemeProvider";
+import { Grid } from "@mui/material";
 const NavbarLatest = dynamic(() => import("./NavBarLatest"), {
-    ssr: false
+    ssr: false,
+    loading: () =>  <Grid container px={{ lg: 14, xs: 3, md: 6 }} py={2}></Grid>
 })
 const FooterLatest = dynamic(() => import("./FooterLatest"), {
     ssr: false
