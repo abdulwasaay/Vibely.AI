467550e27fc1c5756de456e28501a6f86a45aadf
abdul wasay <wasaysaleem071@gmail.com>
2025-07-29 13:36:07 -0700

    dones

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -6,7 +6,6 @@ import ClientThemeProvider from "./parentThemeComponent/clientThemeProvider";
 import { Grid } from "@mui/material";
 const NavbarLatest = dynamic(() => import("./NavBarLatest"), {
     ssr: false,
-    loading: () =>  <Grid container px={{ lg: 14, xs: 3, md: 6 }} py={2}>loading</Grid>
 })
 const FooterLatest = dynamic(() => import("./FooterLatest"), {
     ssr: false
