d6705e13949da40af13aa28a23330ca2b1f62b87
abdul wasay <wasaysaleem071@gmail.com>
2025-08-02 09:38:04 -0700

    done

--- backend/app.js
+++ backend/app.js
@@ -3,18 +3,24 @@ const { port, mongodb_url, mongodb_name } = require('./config/env');
 const { corsOptions } = require('./config/corsOptions');
 const cors = require('cors');
 const mongoConnection = require('./services/mongoConnection');
-const { default: mongoose } = require('mongoose');
+const users = require('./models/user');
 const app = express();
 app.use(cors(corsOptions));
+mongoConnection(mongodb_url, mongodb_name)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', async (req, res) => {
+  try {
+    const getName = await users.create({
+      name: "waay"
+    });
+    console.log("✅ Inserted:", getName);
+    res.send('Hello World!');
+  } catch (err) {
+    console.error("❌ Insert Error:", err.message);
+    res.status(500).send("Insert Failed");
+  }
 });
 
-mongoConnection(mongodb_url, mongodb_name).then(() => {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-}).catch((err) => {
-  console.log(err)
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
