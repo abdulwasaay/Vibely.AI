ac481a538865870dd76999183cd46978534f7a85
abdul wasay <wasaysaleem071@gmail.com>
2025-08-02 08:56:52 -0700

    testing

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,14 @@ const { port, mongodb_url, mongodb_name } = require('./config/env');
 const { corsOptions } = require('./config/corsOptions');
 const cors = require('cors');
 const mongoConnection = require('./services/mongoConnection');
+const { default: mongoose } = require('mongoose');
 const app = express();
 app.use(cors(corsOptions));
 
 mongoConnection(mongodb_url , mongodb_name)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!' + mongoose.connection.readyState);
 });
 
 app.listen(port, () => {
