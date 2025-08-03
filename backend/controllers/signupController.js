9bdd34a2c4063375755a2a1a60d01eaf19ce89db
abdul wasay <wasaysaleem071@gmail.com>
2025-08-03 07:20:25 -0700

    done

--- backend/controllers/signupController.js
+++ backend/controllers/signupController.js
@@ -4,20 +4,20 @@ const { Hashing } = require("../services/hashing");
 const signupController = async (req, res) => {
     const { email, userName, password } = req.body;
     try {
-        const users = await user.findOne({ email });
-        const username = await user.findOne({ userName });
-        if (users) {
-            return res.status(400).send({ message: "User already exists!" });
-        }
-        if (username) {
-            return res.status(400).send({ message: "Username already exists!" });
-        }
+        // const users = await user.findOne({ email });
+        // const username = await user.findOne({ userName });
+        // if (users) {
+        //     return res.status(400).send({ message: "User already exists!" });
+        // }
+        // if (username) {
+        //     return res.status(400).send({ message: "Username already exists!" });
+        // }
         // const hashedPassword = await Hashing(password)
-        await user.create({
-            userName,
-            email,
-            password
-        })
+        // await user.create({
+        //     userName,
+        //     email,
+        //     password: hashedPassword
+        // })
         return res.status(200).send({ message: "User Registered Successfully" });
 
     } catch (err) {
