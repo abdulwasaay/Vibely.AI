cf98b0590e3228f4f88e0db3a2641ac0b392f3fc
abdul wasay <wasaysaleem071@gmail.com>
2025-08-03 06:47:49 -0700

    done

--- backend/services/mongoConnection.js
+++ backend/services/mongoConnection.js
@@ -6,7 +6,6 @@ const mongoConnection = async (url) => {
         console.log(`✅ MongoDB Connected: ${url}`);
     } catch (err) {
         console.error("❌ MongoDB Connection Failed:", err.message);
-        process.exit(1); // Exit process with failure
     }
 };
 
