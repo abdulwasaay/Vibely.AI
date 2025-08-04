555f1af73c00ea73511c00c660b71b86e27d1b6c
abdul wasay <wasaysaleem071@gmail.com>
2025-08-04 10:33:40 -0700

    waiting

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -2,13 +2,19 @@ const express = require("express");
 const { UserNameValidator, EmailValidator, PasswordValidator } = require("../services/validators/UserFieldsValidator");
 const runValidation = require("../services/Validate");
 const signupController = require("../controllers/authControllers/signupController");
+const { default: forgetController } = require("../controllers/authControllers/forgetController");
 const router = express.Router();
 
-const validations = [
+const signupValidations = [
     UserNameValidator(),
     EmailValidator(),
     PasswordValidator()
 ]
-router.post("/signup", runValidation(validations), signupController)
+
+const forgetPassowrdValidations = [
+    EmailValidator()
+]
+router.post("/signup", runValidation(signupValidations), signupController)
+router.post("/forgotpassword", runValidation(forgetPassowrdValidations), forgetController)
 
 module.exports = router
