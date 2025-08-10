3b19cdb8e26dbf3d0490cf4bdb220776959f1355
abdul wasay <wasaysaleem071@gmail.com>
2025-08-10 19:18:25 +0500

    ok

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -6,7 +6,7 @@ import { toast } from "react-toastify";
 
 const api = axios.create({
     baseURL: `${base_url}/api/`,
-    timeout: 10000,
+    timeout: 20000,
     headers: {
         "Content-Type": "application/json"
     },
